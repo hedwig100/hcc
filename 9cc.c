e58ad2adb17baca0ceec99951b96dd2f20458d41
hedwig100 <pentagon7951@outlook.jp>
2021-10-09 18:20:07 +0900

    add tokenizer

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,106 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <ctype.h> 
+#include <stdarg.h> 
+#include <stdbool.h> 
+#include <string.h> 
+
+typedef enum { // enum型
+    TK_RESERVED, // operator
+    TK_NUM, // integer 
+    TK_EOF, // end of input
+} TokenKind; 
+
+typedef struct Token Token; 
+
+struct Token {
+    TokenKind kind; // token kind 
+    Token *next; // next input token 
+    int val; // if kind is TK_NUM,number
+    char *str; // token string 
+}; 
+
+Token *token; 
+
+void error(char *fmt, ...) {
+    va_list ap; // 可変引数
+    va_start(ap,fmt); 
+    vfprintf(stderr,fmt,ap); 
+    fprintf(stderr,"\n");
+    exit(1);
+}
+
+// 次のトークンが期待している記号の時にはトークンを一つ読み進めて真を返す。それ以外の場合は偽を返す
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        return false; 
+    }
+    token = token->next;
+    return true; 
+}
+
+// 次のトークンが期待している記号の時はトークンを一つ読み進める
+// それ以外の場合はエラーを報告する
+bool expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        error("'%c'ではありません"); 
+    }
+    token = token->next; 
+}
+
+// 次のトークンが数値の場合､トークンを一つ読み進めてその数値を返す
+// それ以外の場合はエラーを報告する
+
+int expect_number() {
+    if (token->kind != TK_NUM) {
+        error("数ではありません"); 
+    }
+    int val = token->val; 
+    token = token->next; 
+    return val; 
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurにつなげる
+Token *new_token(TokenKind kind,Token *cur,char *str) {
+    Token* tok = calloc(1,sizeof(Token)); 
+    tok->kind = kind; 
+    tok->str = str; 
+    cur->next = tok; 
+    return tok; 
+}
+
+Token *tokenize(char *p) {
+    Token head; 
+    head.next = NULL; 
+    Token *cur = &head; 
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++; 
+            continue; 
+        }
+
+        if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED,cur,p++); 
+            continue; 
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM,cur,p); 
+            cur->val = strtol(p,&p,10); 
+            continue; 
+        }
+
+        error("cannot tokenize");
+    }
+
+    new_token(TK_EOF,cur,p); 
+    return head.next; 
+}
 
 int main(int argc,char **argv) {
     if (argc != 2) {
@@ -7,28 +108,23 @@ int main(int argc,char **argv) {
         return 1; 
     }
 
-    char *p = argv[1]; 
+    token = tokenize(argv[1]);  
 
     printf(".intel_syntax noprefix\n"); 
     printf(".globl main\n"); 
-    printf("main:\n"); 
-    printf("    mov rax, %ld\n",strtol(p,&p,10)); 
+    printf("main:\n");
 
-    while (*p) {
-        if (*p == '+') {
-            p++; 
-            printf("    add rax, %ld\n",strtol(p,&p,10)); 
+    // 最初は数である必要がある
+    printf("    mov rax, %d\n",expect_number()); 
+
+    while (!at_eof()) {
+        if (consume('+')) {
+            printf("    add rax, %d\n",expect_number()); 
             continue; 
         }
 
-        if (*p == '-') {
-            p++; 
-            printf("    sub rax, %ld\n",strtol(p,&p,10)); 
-            continue; 
-        }
-
-        fprintf(stderr,"unexpected character!: '%c'\n",*p); 
-        return 1;
+        expect('-'); 
+        printf("    sub rax, %d\n",expect_number()); 
     }
 
     printf("    ret\n");
--- test.sh
+++ test.sh
@@ -19,4 +19,5 @@ assert() {
 assert 0 0 
 assert 42 42 
 assert 21 "5+20-4"
+assert 24 "4 - 10 + 30"
 echo OK 
