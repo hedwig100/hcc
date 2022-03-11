4ff7eb45fe4b6d5f6f5105a26887ee8b3a8000ff
hedwig100 <pentagon7951@outlook.jp>
2022-03-11 06:55:09 +0000

    [add] typedef test

--- test/type.c
+++ test/type.c
@@ -3,6 +3,7 @@
 typedef int A;
 typedef int *B;
 typedef struct Node Node;
+typedef int C[3];
 
 struct Node {
     Node *next;
@@ -21,6 +22,13 @@ int main() {
     node->label = 10;
     ASSERT(10, z.label);
     ASSERT(10, (node->next)->label);
+    C arr[5] = {{0, 1, 2}, {3, 4, 5}};
+    ASSERT(0, arr[0][0]);
+    ASSERT(1, arr[0][1]);
+    ASSERT(2, arr[0][2]);
+    ASSERT(3, arr[1][0]);
+    ASSERT(4, arr[1][1]);
+    ASSERT(5, arr[1][2]);
     ok();
     return 0;
 }
